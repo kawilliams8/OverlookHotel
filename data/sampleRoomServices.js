158602a5f50f27d9c85c943d604a54a9742f8087
Katie Williams <kawilliams8@gmail.com>
2019-07-24 15:02:17 -0600

    Add sample data

---
+++ data/sampleRoomServices.js
@@ -0,0 +1,304 @@
+let sampleRoomServices = [
+  {
+    userID: 14,
+    date: "2019/07/29",
+    food: "Rustic Concrete Sandwich",
+    totalCost: 14.9
+  },
+  {
+    userID: 100,
+    date: "2019/10/18",
+    food: "Rustic Cotton Sandwich",
+    totalCost: 17.33
+  },
+  {
+    userID: 92,
+    date: "2019/09/25",
+    food: "Tasty Wooden Sandwich",
+    totalCost: 11.15
+  },
+  {
+    userID: 8,
+    date: "2019/08/02",
+    food: "Practical Granite Sandwich",
+    totalCost: 14.87
+  },
+  {
+    userID: 48,
+    date: "2019/09/26",
+    food: "Fantastic Cotton Sandwich",
+    totalCost: 17.61
+  },
+  {
+    userID: 23,
+    date: "2019/09/11",
+    food: "Awesome Cotton Sandwich",
+    totalCost: 20.79
+  },
+  {
+    userID: 12,
+    date: "2019/10/25",
+    food: "Refined Metal Sandwich",
+    totalCost: 12.32
+  },
+  {
+    userID: 86,
+    date: "2019/09/16",
+    food: "Incredible Concrete Sandwich",
+    totalCost: 24.77
+  },
+  {
+    userID: 55,
+    date: "2019/10/18",
+    food: "Unbranded Wooden Sandwich",
+    totalCost: 7.95
+  },
+  {
+    userID: 55,
+    date: "2019/10/17",
+    food: "Intelligent Fresh Sandwich",
+    totalCost: 12.32
+  },
+  {
+    userID: 97,
+    date: "2019/08/06",
+    food: "Unbranded Wooden Sandwich",
+    totalCost: 12.83
+  },
+  {
+    userID: 88,
+    date: "2019/08/11",
+    food: "Handcrafted Rubber Sandwich",
+    totalCost: 22.45
+  },
+  {
+    userID: 36,
+    date: "2019/08/17",
+    food: "Tasty Granite Sandwich",
+    totalCost: 18.73
+  },
+  {
+    userID: 30,
+    date: "2019/08/12",
+    food: "Refined Cotton Sandwich",
+    totalCost: 12.65
+  },
+  {
+    userID: 13,
+    date: "2019/10/01",
+    food: "Handcrafted Metal Sandwich",
+    totalCost: 12.36
+  },
+  {
+    userID: 3,
+    date: "2019/10/23",
+    food: "Rustic Soft Sandwich",
+    totalCost: 6.78
+  },
+  {
+    userID: 85,
+    date: "2019/10/22",
+    food: "Incredible Cotton Sandwich",
+    totalCost: 8.26
+  },
+  {
+    userID: 99,
+    date: "2019/08/24",
+    food: "Awesome Soft Sandwich",
+    totalCost: 9.63
+  },
+  {
+    userID: 46,
+    date: "2019/09/14",
+    food: "Generic Wooden Sandwich",
+    totalCost: 10.63
+  },
+  {
+    userID: 87,
+    date: "2019/08/08",
+    food: "Refined Plastic Sandwich",
+    totalCost: 7.47
+  },
+  {
+    userID: 48,
+    date: "2019/09/12",
+    food: "Rustic Metal Sandwich",
+    totalCost: 14.95
+  },
+  {
+    userID: 69,
+    date: "2019/07/31",
+    food: "Incredible Granite Sandwich",
+    totalCost: 9.28
+  },
+  {
+    userID: 5,
+    date: "2019/09/08",
+    food: "Sleek Steel Sandwich",
+    totalCost: 12.79
+  },
+  {
+    userID: 87,
+    date: "2019/09/14",
+    food: "Intelligent Fresh Sandwich",
+    totalCost: 19.44
+  },
+  {
+    userID: 19,
+    date: "2019/10/27",
+    food: "Generic Cotton Sandwich",
+    totalCost: 21.66
+  },
+  {
+    userID: 59,
+    date: "2019/08/01",
+    food: "Licensed Plastic Sandwich",
+    totalCost: 10.64
+  },
+  {
+    userID: 31,
+    date: "2019/10/26",
+    food: "Generic Plastic Sandwich",
+    totalCost: 18.34
+  },
+  {
+    userID: 63,
+    date: "2019/08/10",
+    food: "Sleek Frozen Sandwich",
+    totalCost: 15.24
+  },
+  {
+    userID: 80,
+    date: "2019/09/03",
+    food: "Rustic Plastic Sandwich",
+    totalCost: 9.94
+  },
+  {
+    userID: 97,
+    date: "2019/08/06",
+    food: "Tasty Granite Sandwich",
+    totalCost: 20.84
+  },
+  {
+    userID: 43,
+    date: "2019/09/10",
+    food: "Licensed Cotton Sandwich",
+    totalCost: 14.74
+  },
+  {
+    userID: 1,
+    date: "2019/09/28",
+    food: "Refined Rubber Sandwich",
+    totalCost: 9.89
+  },
+  {
+    userID: 56,
+    date: "2019/10/26",
+    food: "Tasty Soft Sandwich",
+    totalCost: 23.7
+  },
+  {
+    userID: 41,
+    date: "2019/09/07",
+    food: "Ergonomic Metal Sandwich",
+    totalCost: 19.14
+  },
+  {
+    userID: 80,
+    date: "2019/09/10",
+    food: "Licensed Frozen Sandwich",
+    totalCost: 21.75
+  },
+  {
+    userID: 86,
+    date: "2019/08/29",
+    food: "Awesome Soft Sandwich",
+    totalCost: 9.06
+  },
+  {
+    userID: 16,
+    date: "2019/09/04",
+    food: "Fantastic Metal Sandwich",
+    totalCost: 21.6
+  },
+  {
+    userID: 48,
+    date: "2019/08/14",
+    food: "Handmade Concrete Sandwich",
+    totalCost: 18.49
+  },
+  {
+    userID: 33,
+    date: "2019/10/13",
+    food: "Handmade Steel Sandwich",
+    totalCost: 8.84
+  },
+  {
+    userID: 72,
+    date: "2019/08/27",
+    food: "Small Cotton Sandwich",
+    totalCost: 22.94
+  },
+  {
+    userID: 80,
+    date: "2019/09/09",
+    food: "Intelligent Granite Sandwich",
+    totalCost: 18.11
+  },
+  {
+    userID: 53,
+    date: "2019/08/08",
+    food: "Practical Fresh Sandwich",
+    totalCost: 21.51
+  },
+  {
+    userID: 23,
+    date: "2019/09/21",
+    food: "Practical Steel Sandwich",
+    totalCost: 15.85
+  },
+  {
+    userID: 78,
+    date: "2019/10/28",
+    food: "Handmade Concrete Sandwich",
+    totalCost: 12.28
+  },
+  {
+    userID: 28,
+    date: "2019/08/27",
+    food: "Intelligent Wooden Sandwich",
+    totalCost: 19.3
+  },
+  {
+    userID: 84,
+    date: "2019/10/06",
+    food: "Handmade Soft Sandwich",
+    totalCost: 5.32
+  },
+  {
+    userID: 56,
+    date: "2019/08/01",
+    food: "Handcrafted Cotton Sandwich",
+    totalCost: 6.27
+  },
+  {
+    userID: 13,
+    date: "2019/10/22",
+    food: "Small Metal Sandwich",
+    totalCost: 22.75
+  },
+  {
+    userID: 33,
+    date: "2019/10/09",
+    food: "Gorgeous Soft Sandwich",
+    totalCost: 6.65
+  },
+  {
+    userID: 34,
+    date: "2019/09/29",
+    food: "Sleek Metal Sandwich",
+    totalCost: 11.61
+  }
+]
+
+export default sampleRoomServices;
---
+++ data/sampleUsers.js
@@ -0,0 +1,64 @@
+let sampleUsers = [
+  {
+    id: 1,
+    name: "Matilde Larson"
+  },
+  {
+    id: 2,
+    name: "Chadrick Lowe"
+  },
+  {
+    id: 3,
+    name: "Christian Sporer"
+  },
+  {
+    id: 4,
+    name: "Brook Christiansen"
+  },
+  {
+    id: 5,
+    name: "Noemy Little"
+  },
+  {
+    id: 6,
+    name: "Winnifred Kris"
+  },
+  {
+    id: 7,
+    name: "Josianne Huels"
+  },
+  {
+    id: 8,
+    name: "Zachery Abbott"
+  },
+  {
+    id: 9,
+    name: "Paula Anderson"
+  },
+  {
+    id: 10,
+    name: "Chyna Gulgowski"
+  },
+  {
+    id: 11,
+    name: "Amiya Effertz"
+  },
+  {
+    id: 12,
+    name: "Leland Roberts"
+  },
+  {
+    id: 13,
+    name: "Cleveland Schimmel"
+  },
+  {
+    id: 14,
+    name: "Juanita Lakin"
+  },
+  {
+    id: 15,
+    name: "Sammie Fahey"
+  }
+]
+
+export default sampleUsers;
