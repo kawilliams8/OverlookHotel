795a7b2536c9b91ece8f7e40c85a3742507b5291
Katherine Williams <kawilliams8@gmail.com>
2019-07-24 21:03:17 -0600

    Add basic splash, spinner

--- src/index.js
+++ src/index.js
@@ -2,30 +2,30 @@ import $ from 'jquery';
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png';
+import './images/spinner.gif';
 
 import Hotel from '../src/Hotel';
 
 let hotel, bookings, rooms, roomServices, users;
 fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings")
-.then(response => response.json())
-.then(dataset => bookings = dataset.bookings)
-.catch(error => console.log(error))
+  .then(response => response.json())
+  .then(dataset => bookings = dataset.bookings)
+  .catch(error => console.log(error))
 
 fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices")
-.then(response => response.json())
-.then(dataset => roomServices = dataset.roomServices)
-.catch(error => console.log(error))
+  .then(response => response.json())
+  .then(dataset => roomServices = dataset.roomServices)
+  .catch(error => console.log(error))
 
 fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users")
-.then(response => response.json())
-.then(dataset => users = dataset.users)
-.catch(error => console.log(error));
+  .then(response => response.json())
+  .then(dataset => users = dataset.users)
+  .catch(error => console.log(error));
 
 fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms")
-.then(response => response.json())
-.then(dataset => rooms = dataset.rooms)
-.catch(error => console.log(error))
+  .then(response => response.json())
+  .then(dataset => rooms = dataset.rooms)
+  .catch(error => console.log(error))
 
 setTimeout(() => {
   hotel = new Hotel(users, rooms, bookings, roomServices);
@@ -35,5 +35,10 @@ setTimeout(() => {
 
 
 $(document).ready(() => {
-  
+  $('main').hide();
 });
+
+$('.splash-button').on('click', () => {
+  $('main').show();
+  $('.splash-div').hide();
+})
